ea955f2861ebf13fb2aeb02508d6c4aa3828caaa
Andrzej Miedzinski <andrzej.miedzinski@sap.com>
2025-02-26 21:24:09 +0100

    Test

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,11 @@ import { getJestProjectsAsync } from '@nx/jest';
 
 export default async () => ({
   projects: await getJestProjectsAsync(),
+  reporters: [
+    'default',
+    ['jest-allure', {
+      resultsDir: './allure-results'
+    }]
+  ]
 });
+
